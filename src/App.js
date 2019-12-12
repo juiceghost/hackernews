13c59c521b75fb33a36075401fdfa1a3fc9fe3de
juiceghost <jg@renegade.se>
2019-12-12 11:10:25 +0100

    Broke out components from App and started refactor into FSC

--- src/App.js
+++ src/App.js
@@ -47,36 +47,79 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App" >
-        <form>
-          <input
-            type="text"
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {
-          this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={item.objectID}><span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                > Dismiss
-              </button>
-              </span>
-              {/* {this.state.isItFriday ? "It's friday!" : "Not Friday"} */}
-            </div>
-          )
-        }
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >Search
+          </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
 }
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    /> </form>
 
+/* class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children} <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        /> </form>
+    );
+  }
+} */
+
+// TODO: Refactor Table to a FSC (Functional Stateless Component)
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
+            </span>
+          </div>)}
+      </div>);
+  }
+}
+//TODO: Refactor
+class Button extends Component {
+  render() {
+    const { onClick, className = '', children,
+    } = this.props;
+    return (<button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >{children}
+    </button>);
+  }
+}
 export default App;
